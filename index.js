26da65f4e2d3cda45fb14c20ed762379ffb0c548
Sarthak <sarthakss898@gmail.com>
2025-06-11 01:11:30 +0530

    Added random username generation and online users count

--- index.js
+++ index.js
@@ -5,20 +5,36 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+function generateUsername() {
+    const adjectives = ['Happy', 'Clever', 'Brave', 'Swift', 'Bright', 'Calm', 'Eager', 'Fierce', 'Gentle', 'Jolly', 
+                      'Lively', 'Mighty', 'Nimble', 'Proud', 'Quick', 'Royal', 'Smart', 'Tender', 'Vivid', 'Wise'];
+    const nouns = ['Panda', 'Tiger', 'Eagle', 'Dolphin', 'Wolf', 'Lion', 'Fox', 'Bear', 'Hawk', 'Shark',
+                  'Dragon', 'Phoenix', 'Falcon', 'Lynx', 'Puma', 'Jaguar', 'Leopard', 'Panther', 'Cobra', 'Python'];
+    const number = Math.floor(Math.random() * 10000);
+    
+    const randomAdj = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    
+    return `${randomAdj}${randomNoun}${number}`;
+}
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection',(socket)=>{
     console.log("A user connected");
-    socket.on('message',(message)=>{
-        io.emit('message',message);
-        console.log(message);
+    io.emit('newuser',io.engine.clientsCount)
+    io.emit('send',{'message':"has joined the chat",'username':'A new user'})
+    socket.on('send',(send)=>{
+        io.emit('send',send);
     })
     socket.on('disconnect',()=>{
-        console.log("A user disconnected");
+        io.emit('send',{'message':"has left the chat",'username':'A user'})
+        io.emit('newuser',io.engine.clientsCount)
     })
 })
+
 
 server.listen(3000, '0.0.0.0', () => {
     console.log("Listening on 3000...");
