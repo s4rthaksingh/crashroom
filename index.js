89b2974bd5427dad010add857cd35d530d87322b
Sarthak <sarthakss898@gmail.com>
2025-06-11 15:15:35 +0530

    Preparing for vercel v2

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ app.get('/',(req,res)=>{
 })
 
 io.on('connection',(socket)=>{
-    console.log("A user connected");
     io.emit('newuser',io.engine.clientsCount)
     io.emit('send',{'message':"has joined the chat",'username':'A new user'})
     socket.on('send',(send)=>{
@@ -40,7 +39,7 @@ io.on('connection',(socket)=>{
     })
 })
 
-const port = process.env.port || 3000;
-server.listen(port, '0.0.0.0', () => {
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
     console.log(`Listening on ${port}...`);
 })
