86b7050971f8ec91c115f4eb3ab342060d633383
Sarthak <sarthakss898@gmail.com>
2025-06-11 18:19:30 +0530

    Notify when someone enters/exits/changes username

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function generateUsername(){
+    const adjectives = ['Happy', 'Clever', 'Brave', 'Swift', 'Bright', 'Calm', 'Eager', 'Fierce', 'Gentle', 'Jolly', 
+        'Lively', 'Mighty', 'Nimble', 'Proud', 'Quick', 'Royal', 'Smart', 'Tender', 'Vivid', 'Wise'];
+    const nouns = ['Panda', 'Tiger', 'Eagle', 'Dolphin', 'Wolf', 'Lion', 'Fox', 'Bear', 'Hawk', 'Shark',
+    'Dragon', 'Phoenix', 'Falcon', 'Lynx', 'Puma', 'Jaguar', 'Leopard', 'Panther', 'Cobra', 'Python'];
+
+    const number = Math.floor(Math.random() * 10000);
+    const randomAdj = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    const generatedUsername = `${randomAdj}${randomNoun}${number}`;
+    return(generatedUsername);
+}
+
+
+
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -15,8 +30,29 @@ app.get('/',(req,res)=>{
 })
 
 io.on('connection',(socket)=>{
+    const username = generateUsername();
+    socket.username = username;
+    io.emit('setUsername',username);
+    io.emit('message',{
+        'message':'has joined the chat',
+        'username':socket.username
+    })
+    socket.on('changeUsername',(newusername)=>{
+        io.emit('message',{
+            'message':`has changed their username to ${newusername}`,
+            'username':socket.username
+        })
+        socket.username = newusername;
+    })
     socket.on('message',(message)=>{
+        if (!message.username) message.username = socket.username;
         io.emit('message', message);
+    })
+    socket.on('disconnect',()=>{
+        io.emit('message',{
+            'message':'left the chat',
+            'username': socket.username
+        })
     })
 })
 
